476025e51a164379788381f4aab6ae3a2771761f
Amirhossien Kiani <amirhkiani98@yahoo.com>
2025-03-26 11:40:04 -0500

    Update simulation parameters and log output; modify input files and enhance plot configurations

--- run_command.py
+++ run_command.py
@@ -9,9 +9,10 @@ main_script = "main.py"
 is_windows = platform.system() == "Windows"
 
 # Loop to run the script with different nu values
-for cutoff in [0]:
+omage = 0.0
+for cutoff in [1, 2]:
     omage = omage/100
-    for i in range(0, 101, 20):
+    for i in range(0, 91, 30):
         nu_value = i / 100.0
         # Construct the command with the script path and nu_value
         # if is_windows:
