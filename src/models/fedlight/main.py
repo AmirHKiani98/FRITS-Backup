17ed202c179d3bda01d5887c18d2ad9159046d61
Amirhossein Kiani <amirhkiani98@yahoo.com>
2025-06-09 04:46:29 -0500

    Refactor code structure for improved readability and maintainability

--- src/models/fedlight/main.py
+++ src/models/fedlight/main.py
@@ -36,7 +36,7 @@ def main():
 
     parser.add_argument('--net', type=str, default=BASE_DIR + r"/../../networks/4x4.net.xml")
     parser.add_argument('--route', type=str, default=BASE_DIR + r'/../../routes/4x4c2c1.rou.xml')
-    parser.add_argument("--num-episodes", type=int, default=2)
+    parser.add_argument("--num-episodes", type=int, default=5)
     parser.add_argument("--noised-edge", type=str, default="10")
     parser.add_argument("--noise-added", type=float, default=0.1)
     parser.add_argument("--gui", action="store_true")
