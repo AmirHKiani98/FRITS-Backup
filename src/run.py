930d125fca560665e320d22fc100258a09979b4f
Amirhossein Kiani <amirhkiani98@yahoo.com>
2025-06-10 07:30:21 -0500

    Refactor run.py to comment out cutoff processing and update omega and nu parameters

--- src/run.py
+++ src/run.py
@@ -2,25 +2,25 @@ import subprocess
 import os
 
 # With cutoff
-for cutoff in [2, 3, 4]:
-    for nu in [0.0, 0.5, 1.0]:
-        command = [
-            "python", "-m" "src.main",
-            "--cutoff", str(cutoff),
-            "--nu", str(nu)
-        ]
-        print(f"\n➡️ Running: {' '.join(command)}\n")
+# for cutoff in [2, 3, 4]:
+#     for nu in [0.0, 0.5, 1.0]:
+#         command = [
+#             "python", "-m" "src.main",
+#             "--cutoff", str(cutoff),
+#             "--nu", str(nu)
+#         ]
+#         print(f"\n➡️ Running: {' '.join(command)}\n")
     
-        try:
-            subprocess.run(command, check=True)
-        except subprocess.CalledProcessError as e:
-            print(f"❌ Error: Command failed with exit code {e.returncode}")
-        os.system(" ".join(command))
+#         try:
+#             subprocess.run(command, check=True)
+#         except subprocess.CalledProcessError as e:
+#             print(f"❌ Error: Command failed with exit code {e.returncode}")
+#         os.system(" ".join(command))
     
 
 # With omega
-for omega in [0.5, 1.0, 1.5]:
-    for nu in [0.0, 0.5, 1.0]:
+for omega in [0]:
+    for nu in [0.4, 0.5, 0.6]:
         command = [
             "python", "-m" "src.main",
             "--omega", str(omega),
