5b3b8e77810482cc57dd29426482d8b7081626db
Amirhossien Kiani <amirhkiani98@yahoo.com>
2025-07-08 06:46:15 -0500

    Add vehicle waiting time data for attacked scenario with alpha 5, run 2

--- src/run.py
+++ src/run.py
@@ -2,29 +2,12 @@ import subprocess
 import os
 
 # With cutoff
-# for cutoff in [2, 3, 4]:
-#     for nu in [0.0, 0.5, 1.0]:
-#         command = [
-#             "python", "-m", "src.main",
-#             "--cutoff", str(cutoff),
-#             "--nu", str(nu)
-#         ]
-#         print(f"\n➡️ Running: {' '.join(command)}\n")
-    
-#         try:
-#             subprocess.run(command, check=True)
-#         except subprocess.CalledProcessError as e:
-#             print(f"❌ Error: Command failed with exit code {e.returncode}")
-#         os.system(" ".join(command))
-    
-
-# With omega
-for omega in [0]:
-    for nu in [0.4, 0.5, 0.6]:
+for cutoff in [2, 3, 4]:
+    for nu in [0.5]:
         command = [
-            "python", "-m" "src.main",
-            "--omega", str(omega),
-            "--nu", str(nu),
+            "python", "-m", "src.main",
+            "--cutoff", str(cutoff),
+            "--nu", str(nu)
         ]
         print(f"\n➡️ Running: {' '.join(command)}\n")
     
@@ -33,3 +16,20 @@ for omega in [0]:
         except subprocess.CalledProcessError as e:
             print(f"❌ Error: Command failed with exit code {e.returncode}")
         os.system(" ".join(command))
+    
+
+# With omega
+# for omega in [0]:
+#     for nu in [0.4, 0.5, 0.6]:
+#         command = [
+#             "python", "-m" "src.main",
+#             "--omega", str(omega),
+#             "--nu", str(nu),
+#         ]
+#         print(f"\n➡️ Running: {' '.join(command)}\n")
+    
+#         try:
+#             subprocess.run(command, check=True)
+#         except subprocess.CalledProcessError as e:
+#             print(f"❌ Error: Command failed with exit code {e.returncode}")
+#         os.system(" ".join(command))
